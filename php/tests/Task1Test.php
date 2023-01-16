67f1877e6c3031593cdadf5dc218682802eea276
jangaricco <jangareed91@gmail.com>
2023-01-16 17:56:53 +0700

    Task1

--- php/tests/Task1Test.php
+++ php/tests/Task1Test.php
@@ -11,12 +11,12 @@ Class Task1Test extends TestCase {
   public function test_getArea()
   {
     $circle = new Circle(11);
-    $this->assertSame('380.132711084365', $circle->getArea());
+    $this->assertEquals('379.94', $circle->getArea());
   }
 
   public function test_getPrimeter()
   {
     $circle = new Circle(4.44);
-    $this->assertSame('27.897342763877365', $circle->getPerimeter());
+    $this->assertEquals('27.883200000000002', $circle->getPerimeter());
   }
 }
